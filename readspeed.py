ccd543199c4ee423e735f9391b6c170c006acfd6
Rachel <20343175+razh431@users.noreply.github.com>
2020-10-18 19:38:00 -0400

    Update readspeed.py

--- readspeed.py
+++ readspeed.py
@@ -5,16 +5,21 @@ import csv
 import pytesseract
 from PIL import Image
 
+"""use pytesseract, optical character recognition for recognizing anemometer video reading"""
 
+
+#sort the os in terms of numerical order??? why is it different orders from creation
 image_folder = 'flow'
 img_list = os.listdir(image_folder)
 img_list.sort()
 
+#already created image file of images from each video frame
 images = [img for img in img_list if img.endswith(".jpg")]
 
 with open('flow.csv', 'a') as f_out:
     writer = csv.writer(f_out)
 
+    #for all frames within the video, saved as images each, read optical character
     for image in images:
         print(image)
         im = Image.open('flow/'+ image)
